cbed5aad04034ef402a12e9da2d5f279e4399fef
Renaat De Muynck <renaat.demuynck@gmail.com>
2019-02-20 08:28:50 +0100

    Clean up Webpack config files

--- conf/webpack.dev.config.js
+++ conf/webpack.dev.config.js
@@ -1,8 +1,6 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const postcssImport = require('postcss-import');
 const postcssPresetEnv = require('postcss-preset-env');
-const postcssClean = require('postcss-clean');
 
 module.exports = {
   mode: 'development',
@@ -10,54 +8,46 @@ module.exports = {
   entry: {
     'h5p-accounting-journal-entry': './src/index.js'
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                [
-                  '@babel/preset-env', {
-                    useBuiltIns: 'entry',
-                    loose: true
-                  }
-                ]
-              ]
-            }
-          }
-        ]
-      }, {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader
-          }, {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              localIdentName: 'h5p-accounting-journal-entry__[local]',
-              camelCase: true
-            }
-          }, {
-            loader: 'postcss-loader',
-            options: {
-              plugins: [
-                postcssImport(),
-                postcssPresetEnv()
-              ]
-            }
-          }
-        ]
-      }
-    ]
-  },
   optimization: {
     minimize: false
   },
   plugins: [
     new MiniCssExtractPlugin()
-  ]
+  ],
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', {
+              useBuiltIns: 'entry',
+              loose: true
+            }]
+          ]
+        }
+      }]
+    }, {
+      test: /\.css$/,
+      use: [{
+        loader: MiniCssExtractPlugin.loader
+      }, {
+        loader: 'css-loader',
+        options: {
+          modules: true,
+          localIdentName: 'h5p-accounting-journal-entry__[local]',
+          camelCase: true
+        }
+      }, {
+        loader: 'postcss-loader',
+        options: {
+          plugins: [
+            postcssImport(),
+            postcssPresetEnv()
+          ]
+        }
+      }]
+    }]
+  }
 };
--- conf/webpack.prod.config.js
+++ conf/webpack.prod.config.js
@@ -1,4 +1,3 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const postcssImport = require('postcss-import');
 const postcssPresetEnv = require('postcss-preset-env');
@@ -9,54 +8,46 @@ module.exports = {
   entry: {
     'h5p-accounting-journal-entry': './src/index.js'
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                [
-                  '@babel/preset-env', {
-                    useBuiltIns: 'entry',
-                    loose: true
-                  }
-                ]
-              ]
-            }
-          }
-        ]
-      }, {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader
-          }, {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              localIdentName: 'h5p-accounting-journal-entry__[local]__[hash:5]',
-              camelCase: true
-            }
-          }, {
-            loader: 'postcss-loader',
-            options: {
-              plugins: [
-                postcssImport(),
-                postcssPresetEnv(),
-                postcssClean({
-                  level: 2
-                })
-              ]
-            }
-          }
-        ]
-      }
-    ]
-  },
   plugins: [
     new MiniCssExtractPlugin()
-  ]
+  ],
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', {
+              useBuiltIns: 'entry',
+              loose: true
+            }]
+          ]
+        }
+      }]
+    }, {
+      test: /\.css$/,
+      use: [{
+        loader: MiniCssExtractPlugin.loader
+      }, {
+        loader: 'css-loader',
+        options: {
+          modules: true,
+          localIdentName: 'h5p-accounting-journal-entry__[local]__[hash:5]',
+          camelCase: true
+        }
+      }, {
+        loader: 'postcss-loader',
+        options: {
+          plugins: [
+            postcssImport(),
+            postcssPresetEnv(),
+            postcssClean({
+              level: 2
+            })
+          ]
+        }
+      }]
+    }]
+  }
 };
