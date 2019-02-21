f85c2678faebd1bf7089e8102064ba8a8d317e82
Renaat De Muynck <renaat.demuynck@gmail.com>
2019-02-21 11:30:44 +0100

    Move templates to separate file

--- src/journal-entry.js
+++ src/journal-entry.js
@@ -1,75 +1,4 @@
-import styles from './journal-entry.css';
-
-
-const createJournalItemTable = (data) => (`
-  <table class="${styles.journalItem}">
-    <thead>
-      <tr>
-        <th></th>
-        <th>Number</th>
-        <th>Account name</th>
-        <th>Type</th>
-        <th>+/-</th>
-        <th>Debit</th>
-        <th>Credit</th>
-      </tr>
-    </thead>
-    <tbody class="${styles.debit}">
-      ${createJournalTransaction(data, 'debit', 2)}
-      ${createJournalTransaction(data, 'debit')}
-    </tbody>
-    <tbody class="${styles.credit}">
-      ${createJournalTransaction(data, 'credit', 2)}
-      ${createJournalTransaction(data, 'credit')}
-    </tbody>
-    <tfoot>
-        <th class="${styles.totalLabel}" colspan="5">Total:</th>
-        <th class="${styles.totalDebit}">0.00</th>
-        <th class="${styles.totalCredit}">0.00</th>
-    </tfoot>
-  </table>
-`);
-
-const createJournalTransaction = (data, amountType, titleSpan = false) => (`
-  <tr>
-    ${titleSpan ? `
-      <td rowspan="${titleSpan}" class="${styles.title}">
-        ${amountType === 'debit' ? 'Debit' : 'Credit' }
-      </td>
-    ` : ''}
-    <td class="${styles.accountNumber}">
-      <input type="text" />
-    </td>
-    <td class="${styles.accountName}">
-    </td>
-    <td class="${styles.invoiceType}">
-      <select>
-        <option>&mdash;</option>
-        <option value="A">Assets</option>
-        <option value="L">Liabilities</option>
-        <option value="E">Expenses</option>
-        <option value="R">Revenue</option>
-      </select>
-    </td>
-    <td class="${styles.plusMin}">
-      <select>
-        <option></option>
-        <option value="plus">&plus;</option>
-        <option value="minus">&minus;</option>
-      </select>
-    </td>
-    <td class="${styles.amountDebit}">
-      ${amountType === 'debit' ? `
-        <input type="text" />
-      ` : ''}
-    </td>
-    <td class="${styles.amountCredit}">
-      ${amountType === 'credit' ? `
-        <input type="text" />
-      ` : ''}
-    </td>
-  </tr>
-`);
+import { journalItemTemplate } from './templates';
 
 
 export default class {
@@ -92,7 +21,7 @@ export default class {
    */
   attach($container) {
     $container.addClass('h5p-accounting-journal-entry');
-    $container.append(createJournalItemTable());
+    $container.append(journalItemTemplate());
   }
   
 }
---
+++ src/templates.js
@@ -0,0 +1,72 @@
+import styles from './journal-entry.css';
+
+
+export const journalItemTemplate = (data) => (`
+  <table class="${styles.journalItem}">
+    <thead>
+      <tr>
+        <th></th>
+        <th>Number</th>
+        <th>Account name</th>
+        <th>Type</th>
+        <th>+/-</th>
+        <th>Debit</th>
+        <th>Credit</th>
+      </tr>
+    </thead>
+    <tbody class="${styles.debit}">
+      ${journalTransactionTemplate(data, 'debit', 2)}
+      ${journalTransactionTemplate(data, 'debit')}
+    </tbody>
+    <tbody class="${styles.credit}">
+      ${journalTransactionTemplate(data, 'credit', 2)}
+      ${journalTransactionTemplate(data, 'credit')}
+    </tbody>
+    <tfoot>
+        <th class="${styles.totalLabel}" colspan="5">Total:</th>
+        <th class="${styles.totalDebit}">0.00</th>
+        <th class="${styles.totalCredit}">0.00</th>
+    </tfoot>
+  </table>
+`);
+
+export const journalTransactionTemplate = (data, amountType, titleSpan = false) => (`
+  <tr>
+    ${titleSpan ? `
+      <td rowspan="${titleSpan}" class="${styles.title}">
+        ${amountType === 'debit' ? 'Debit' : 'Credit' }
+      </td>
+    ` : ''}
+    <td class="${styles.accountNumber}">
+      <input type="text" />
+    </td>
+    <td class="${styles.accountName}">
+    </td>
+    <td class="${styles.invoiceType}">
+      <select>
+        <option>&mdash;</option>
+        <option value="A">Assets</option>
+        <option value="L">Liabilities</option>
+        <option value="E">Expenses</option>
+        <option value="R">Revenue</option>
+      </select>
+    </td>
+    <td class="${styles.plusMin}">
+      <select>
+        <option></option>
+        <option value="plus">&plus;</option>
+        <option value="minus">&minus;</option>
+      </select>
+    </td>
+    <td class="${styles.amountDebit}">
+      ${amountType === 'debit' ? `
+        <input type="text" />
+      ` : ''}
+    </td>
+    <td class="${styles.amountCredit}">
+      ${amountType === 'credit' ? `
+        <input type="text" />
+      ` : ''}
+    </td>
+  </tr>
+`);
