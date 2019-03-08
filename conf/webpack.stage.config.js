4a14b18ca62f66cfac8d563aac27cb27e77b88ba
Renaat De Muynck <renaat.demuynck@gmail.com>
2019-03-08 10:20:22 +0100

    Add staging config for Webpack

---
+++ conf/webpack.stage.config.js
@@ -0,0 +1,77 @@
+const { EnvironmentPlugin } = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const postcssImport = require('postcss-import');
+const postcssPresetEnv = require('postcss-preset-env');
+const postcssClean = require('postcss-clean');
+
+module.exports = {
+  mode: 'production',
+  devtool: 'source-map',
+  entry: {
+    'h5p-accounting-journal-entry': [
+      '@babel/polyfill',
+      'nodelist-foreach-polyfill',
+      'whatwg-fetch',
+      './src/polyfills.js',
+      './src/index.js'
+    ]
+  },
+  performance: {
+    maxEntrypointSize: 5000000,
+    maxAssetSize: 5000000
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        sourceMap: true
+      })
+    ]
+  },
+  plugins: [
+    new EnvironmentPlugin({
+      NODE_ENV: 'development'
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', {
+              useBuiltIns: 'entry',
+              loose: true
+            }]
+          ]
+        }
+      }]
+    }, {
+      test: /\.css$/,
+      use: [{
+        loader: MiniCssExtractPlugin.loader
+      }, {
+        loader: 'css-loader',
+        options: {
+          modules: true,
+          localIdentName: 'h5p-accounting-journal-entry__[local]',
+          camelCase: 'only'
+        }
+      }, {
+        loader: 'postcss-loader',
+        options: {
+          plugins: [
+            postcssImport(),
+            postcssPresetEnv(),
+            postcssClean({
+              level: 2
+            })
+          ]
+        }
+      }]
+    }]
+  }
+};
