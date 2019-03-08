1f74f1a3751b88a5ae61516f732107f823754e2e
Renaat De Muynck <renaat.demuynck@gmail.com>
2019-03-08 09:17:42 +0100

    fix: Add polyfill for <output> element (for IE11)

--- src/polyfills.js
+++ src/polyfills.js
@@ -17,4 +17,19 @@
       return null;
     };
   }
+  
+  if (window.HTMLOutputElement === undefined) {
+    Object.defineProperty(HTMLUnknownElement.prototype, 'value', {
+      get: function () {
+        if (this.tagName === 'OUTPUT') {
+          return this.textContent;
+        }
+      },
+      set: function (newValue) {
+        if (this.tagName === 'OUTPUT') {
+          this.textContent = newValue;
+        }
+      }
+    });
+  }
 }
