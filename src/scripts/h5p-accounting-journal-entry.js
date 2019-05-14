d076b0590d5c383b8863746447b2e0fe238ce288
Renaat De Muynck <renaat.demuynck@gmail.com>
2019-05-14 11:43:38 +0200

    Implement the H5P Question type contract

--- src/scripts/h5p-accounting-journal-entry.js
+++ src/scripts/h5p-accounting-journal-entry.js
@@ -65,6 +65,83 @@ class AccountingJournalEntry extends H5P.Question {
     this.setContent(container);
   }
 
+  /**
+   * Checks if answers for this task has been given, and the program can proceed to calculate scores.
+   *
+   * @returns {boolean} TRUE if answers have been given, else FALSE.
+   */
+  getAnswerGiven() {
+    //TODO Check if answers were given
+    return true;
+  }
+
+  /**
+   * Calculates the user's score for this task.
+   *
+   * @returns {number} User's score for this task.
+   */
+  getScore() {
+    //TODO Return points
+    return 0;
+  }
+
+  /**
+   * Calculates the maximum amount of points achievable for this task.
+   *
+   * @returns {number} Max score achievable for this task.
+   */
+  getMaxScore() {
+    //TODO Calculate maximum score
+    return 10;
+  }
+
+  /**
+   * Displays the solution(s) for this task, should also hide all buttons.
+   */
+  showSolutions() {
+    //TODO Show solutions
+  }
+
+  /**
+   * Resets the task to its initial state, should also show buttons that were hidden by the `showSolutions()` function.
+   */
+  resetTask() {
+    this.setExplanation();
+    this.removeFeedback();
+    this.hideSolution();
+
+    this.showButton('check-answer');
+    this.hideButton('show-solution');
+    this.hideButton('try-again');
+
+    //TODO Reset data
+  }
+
+  /**
+   * @typedef {Object} XAPIData
+   * @property {Object} statement - Should contain the xAPI statement that is usually sent on 'answered'.
+   * @property {string} statement.object.definition.interactionType - Type of the interaction. Choose from [xAPI spec]{@link https://github.com/adlnet/xAPI-Spec/blob/master/xAPI-Data.md#interaction-types}.
+   * @property {string} statement.object.definition.description - Question text and any additional information to generate the report.
+   * @property {string} statement.object.definition.correctResponsesPattern - A pattern for determining the correct answers of the interaction. See [spec]{@link https://github.com/adlnet/xAPI-Spec/blob/master/xAPI-Data.md#response-patterns}.
+   * @property {string} statement.object.definition - Additional definition properties required for the specific interaction type.
+   * @property {string} statement.result.response - User answers for interaction. See [spec]{@link https://github.com/adlnet/xAPI-Spec/blob/master/xAPI-Data.md#result}.
+   * @property {number} statement.result.score.min - Minimum possible score for task.
+   * @property {number} statement.result.score.raw - Raw user score.
+   * @property {number} statement.result.score.max - Maximum possible score for task.
+   * @property {number} statement.result.score.scaled - Score in percentage.
+   * @property {array} [children] An array of all children's xAPI event. This is implemented by calling getXAPIData() on all children of the content type.
+   */
+
+  /**
+   * Retrieves the xAPI data necessary for generating result reports.
+   *
+   * @returns {XAPIData}
+   */
+  getXAPIData() {
+    //TODO return xAPI data
+    return {};
+  }
+
 }
 
 export default AccountingJournalEntry;
